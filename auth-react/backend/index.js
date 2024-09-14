1aeb68bd1b0e107cde3b9702359b70a7f18cda08
AdamDono <>
2024-09-14 14:42:45 +0200

    database setup

--- auth-react/backend/index.js
+++ auth-react/backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-
+import dotenv from "dotenv";
+dotenv.config()
 const app = express();
 app.get("/", (req, res) => {res.send("Server is Highly Running!");});
 
 app.listen(3000, () =>
+
   console.log('Server is Listening'),
+  
 ); 
-//avxbSUg9G3XJbCAL
